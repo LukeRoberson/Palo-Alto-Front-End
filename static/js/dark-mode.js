d54fa257637b04222ea1f2f5adb00a9471694c96
Luke <lrobertson@lakemac.nsw.gov.au>
2024-07-04 14:11:47 +1000

    Improved light and dark mode, but there are still troubles

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -7,49 +7,26 @@ function applySavedTheme() {
     if (currentTheme === "dark-mode") {
         document.body.classList.add("dark-mode");
         document.body.classList.remove("light-mode");
+        modeToggle.checked = true;
     } else {
         document.body.classList.add("light-mode");
         document.body.classList.remove("dark-mode");
+        modeToggle.checked = false;
     }
-
-    // Ensure the toggle switch matches the current theme
-    updateToggleSwitch(currentTheme);
 }
 
 function toggleDarkMode() {
-    const isChecked = modeToggle.checked; // Assuming modeToggle is your checkbox
-    if (isChecked) {
-        // Enable dark mode
-        applyTheme("dark-mode");
+    if (modeToggle.checked) {
+        document.body.classList.add("dark-mode");
+        document.body.classList.remove("light-mode");
+
+        localStorage.setItem("theme", "dark-mode");
     } else {
-        // Disable dark mode
-        applyTheme("light-mode");
+        document.body.classList.add("light-mode");
+        document.body.classList.remove("dark-mode");
+
+        localStorage.setItem("theme", "light-mode");
     }
-}
-
-function applyTheme(theme) {
-    // Apply theme to body
-    document.body.classList.toggle("dark-mode", theme === "dark-mode");
-    document.body.classList.toggle("light-mode", theme === "light-mode");
-    
-    // Apply theme to .w3-container
-    const containers = document.querySelectorAll('.w3-container');
-    containers.forEach(container => {
-        container.classList.toggle("dark-mode", theme === "dark-mode");
-        container.classList.toggle("light-mode", theme === "light-mode");
-    });
-
-    // Save theme to localStorage
-    localStorage.setItem("theme", theme);
-}
-
-// Update the toggle switch based on the current theme
-function updateToggleSwitch(currentTheme) {
-    // Get the toggle switch
-    const modeToggle = document.getElementById('mode-toggle');
-    
-    // Set the switch
-    modeToggle.checked = currentTheme === "dark-mode";
 }
 
 // Ensure the DOM is fully loaded before executing any script
